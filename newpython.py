e2b0e07c2ccc6c8bba8b9b4681ec808c5e0c8beb
sithuphonenaing43@gmail.com <sithuphonenaing43@gmail.com>
2021-12-20 22:17:22 +0630

    20.12.22 sololearn lessons

--- newpython.py
+++ newpython.py
@@ -2,3 +2,17 @@ print ( "This is second repository file.")
 print ( " Another line print")
 print ("This is line 3")
 print ("this is line 4")
+print (272 % 150)
+
+
+letters = ["a","b","c"]
+for y in letters:
+    print (y+"!")
+
+numx = list(range(10))
+print(numx)
+x = list(range(10,5,-1))
+print(x)
+
+for f in range(5):
+    print("Hello")
---
+++ var mynum = 36;.js
@@ -0,0 +1,13 @@
+var mynum = 36;
+
+function fun1(){
+anothernum = 5
+
+}
+
+function fun2(){
+console.log(mynum+anothernum)
+
+}
+
+console.log(fun2)
